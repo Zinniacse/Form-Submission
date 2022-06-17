15bc0474a8cf764242f51a6ea997c70ba538dba5
unknown <zinnia.cse7862@gmail.com>
2022-06-17 23:38:46 +0600

    link,button,checkbox locate

--- src/main/java/testCases/TC001_FormSubmission.java
+++ src/main/java/testCases/TC001_FormSubmission.java
@@ -22,10 +22,10 @@ public class TC001_FormSubmission extends DriverSetup{
 		driver.findElement(By.id("inputUsername")).sendKeys("Zinnia");
 	
 		driver.findElement(By.name("inputPassword")).sendKeys("zinnia123");
-		Thread.sleep(5000);
+		Thread.sleep(3000);
 		
 		driver.findElement(By.className("signInBtn")).click();
-		Thread.sleep(5000);
+		Thread.sleep(3000);
 		
 
 		System.out.println(driver.findElement(By.cssSelector("p.error")).getText());
@@ -34,21 +34,35 @@ public class TC001_FormSubmission extends DriverSetup{
 		//            tagname#value (if id is given)
 		
         driver.findElement(By.linkText("Forgot your password?")).click();
-    	Thread.sleep(5000);
+    	Thread.sleep(2000);
          
          
     	//tagname (@attribute'value)
     	
     	driver.findElement(By.xpath("//input[@placeholder= 'Name']")).sendKeys("abir");
-    	Thread.sleep(5000);
+    	Thread.sleep(2000);
     	
     	driver.findElement(By.xpath("//input[@placeholder= 'Email']")).sendKeys("test@gmail.com");
-    	Thread.sleep(5000);
+    	Thread.sleep(2000);
     	
     	driver.findElement(By.xpath("//input[@type= 'text'][3]")).sendKeys("01747106456");
-    	Thread.sleep(5000);
+    	Thread.sleep(2000);
     	
-    
+    	
+    	driver.findElement(By.cssSelector("button.reset-pwd-btn")).click();
+    	Thread.sleep(2000);
+       	
+    	driver.findElement(By.xpath("//div[@class='forgot-pwd-btn-conainer']/button[1]")).click();
+    	Thread.sleep(2000);
+    	
+    	driver.findElement(By.cssSelector("#inputUsername")).sendKeys("Tanvir");
+		Thread.sleep(2000);
+		driver.findElement(By.cssSelector("input[type*='pass']")).sendKeys("rahulshettyacademy");
+		Thread.sleep(2000);
+		driver.findElement(By.id("chkboxOne")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[contains(@class,'submit')]")).click();
+		Thread.sleep(2000);
 	}
 }
 
